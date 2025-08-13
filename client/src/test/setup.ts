a35f31480e92e42e9d2bc6c8d0a8d19f070503e4
Aniket Pandey <aniket00736@gmail.com>
2025-08-13 15:43:38 +0530

    chore(client): remove Firebase; clean test setup

--- client/src/test/setup.ts
+++ client/src/test/setup.ts
@@ -16,12 +16,7 @@ Object.defineProperty(window, 'matchMedia', {
   })),
 });
 
-// Mock Firebase since it's not available in test environment
-vi.mock('../firebase', () => ({
-  getTokenFromFirebase: vi.fn(() => Promise.resolve('mock-token')),
-  onMessageListener: vi.fn(() => Promise.resolve({})),
-  messaging: {},
-}));
+// Removed Firebase mocks (Firebase not used)
 
 // Mock matchMedia
 Object.defineProperty(window, 'matchMedia', {
@@ -38,16 +33,7 @@ Object.defineProperty(window, 'matchMedia', {
   })),
 });
 
-// Mock environment variables
-Object.assign(process.env, {
-  VITE_FIREBASE_API_KEY: 'test-api-key',
-  VITE_FIREBASE_AUTH_DOMAIN: 'test.firebaseapp.com',
-  VITE_FIREBASE_PROJECT_ID: 'test-project',
-  VITE_FIREBASE_STORAGE_BUCKET: 'test.appspot.com',
-  VITE_FIREBASE_MESSAGING_SENDER_ID: '123456789',
-  VITE_FIREBASE_APP_ID: 'test-app-id',
-  VITE_FIREBASE_VAPID_KEY: 'test-vapid-key',
-});
+// Removed Firebase-related env var mocks
 
 // Mock localStorage
 Object.defineProperty(window, 'localStorage', {
